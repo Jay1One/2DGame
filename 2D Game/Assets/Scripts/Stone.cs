7d4b6058ead4279fd17b9313d773f7c45353ad09
Artem <artem0rusakov@@gmail.com>
2020-05-31 16:40:09 +0300

    homework 4 done

---
+++ 2D Game/Assets/Scripts/Stone.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stone : MonoBehaviour, IHitbox
+{
+    [SerializeField] private Rigidbody2D rigidbody2D;
+    private void Start()
+    {
+        Destroy(gameObject,4f);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        
+        //нанести урон игроку если скороть камня большая
+        if (other.gameObject.GetComponent<Player>()!=null && rigidbody2D.velocity.magnitude>0.01f)
+        {
+            other.gameObject.GetComponent<Player>().Hit(1);
+            Destroy(gameObject);
+        }
+        
+        //чтобы не отскакивать слишком много раз от эффектора
+        if (other.collider.usedByEffector)
+        {
+            gameObject.layer = default;
+        }
+    }
+
+    public int Health { get; }
+    public void Hit(int damage)
+    {
+        Die();
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ 2D Game/Assets/Scripts/Stonefall.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+using Random = UnityEngine.Random;
+
+public class Stonefall : MonoBehaviour
+{
+    [SerializeField] private GameObject m_StonePrefab;
+    [SerializeField] private float m_MinStonefallInterval = 0.5f;
+    [SerializeField] private float m_MaxStonefallInterval = 2f;
+    private float nextStoneTimer = 0f;
+    private bool isSeenByCamera;
+
+    private void Start()
+    {
+        nextStoneTimer = Random.Range(m_MinStonefallInterval, m_MaxStonefallInterval) + Time.time;
+    }
+
+
+    private void Update()
+    {
+        if (Time.time >= nextStoneTimer)
+        {
+            Instantiate(m_StonePrefab,transform.position,quaternion.identity);
+            nextStoneTimer = Random.Range(m_MinStonefallInterval, m_MaxStonefallInterval) + Time.time; 
+        }
+    }
+}
